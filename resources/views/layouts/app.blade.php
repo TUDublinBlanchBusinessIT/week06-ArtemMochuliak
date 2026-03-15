b91d09f686231e55e7951a5d61b6b63795c355b8
Natalia Lishchenko <B00165618@mytudublin.ie>
2026-03-15 18:34:24 +0000

    Added product images and display them in product grid

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+</body>
+</html>
